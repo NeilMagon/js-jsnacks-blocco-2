4537be0885d1db946440301506036c4c4dc62491
NeilMagon <neil.magon97@gmail.com>
2024-03-12 20:53:16 +0100

    feat: add const and if

--- js-snack-1/js/script.js
+++ js-snack-1/js/script.js
@@ -1,3 +1,11 @@
 // - L'utente inserisce un numero nel prompt, se è pari stampa il numero,
 // se è dispari stampa il numero successivo
 
+const userNumber = parseInt(prompt(`Dimmi un numero`));
+
+let number = userNumber;
+
+if (userNumber % 2 !== 0) {
+    number++;
+}
+console.log(number);
